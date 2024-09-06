23b48386be606c86e9a556c35e2e4ad07146ce39
M PRAVEEN KUMAR <praveenmurugesan02@gmail.com>
2024-09-06 17:29:42 +0530

    ll1 commit

--- src/main/java/assignment_5Sept/SingleLinkedList.java
+++ src/main/java/assignment_5Sept/SingleLinkedList.java
@@ -1,8 +1,8 @@
 package assignment_5Sept;
-
+import java.util.*;
 public class SingleLinkedList {
 
-    private class Node {
+    private static class Node {
         Object item;
         Node next;
 
@@ -60,34 +60,45 @@ public class SingleLinkedList {
 
     }
 
-    public boolean add(Object e) {
-        addLast(e);
-        return true;
+    public void add(Object e, int index) {
+        if(index == 0){
+            addFirst(e);
+            return;
+        }
+        if(index==size){
+            addLast(e);
+            return;
+        }
+        Node current = first;
+        for(int i=0; i<index-1; i++){
+            current=current.next;
+        }
+        Node node = new Node(e, current.next);
+        current.next = node;
+        size++;
     }
 
 
 
-    public Object removeFirst() {
+    public void removeFirst() {
         if(first == null){
             throw new NoSuchElementException("List is empty");
         }
-        Object item = first.item;
         first = first.next;
         if(first==null) {
             last = null;
         }
         size--;
-        return item;
     }
 
-    public Object removeLast() {
+    public void removeLast() {
         if(last==null){
             throw new NoSuchElementException("List is empty");
         }
         Object item = last.item;
         if(first==last) {
             removeFirst();
-            return item;
+            return;
         }
         Node current = first;
         while(current.next != null){
@@ -99,10 +110,98 @@ public class SingleLinkedList {
             current = current.next;
         }
         size--;
-        return item;
     }
 
-    public void printList(){
+    public void removeElementFirstOcc(Object e) {
+        if(first == null){
+            throw new NoSuchElementException("List is empty");
+        }
+        if(first.item.equals(e)){
+            removeFirst();
+            return;
+        }
+        Node current = first;
+        while(current.next!= null){
+            if(current.next.item.equals(e)){
+                current.next = current.next.next;
+                if(current.next==null) {
+                    last = current;
+                }
+                size--;
+                return;
+            }
+            current = current.next;
+        }
+    }
+
+    public void removeElementLastOcc(Object e) {
+        if(last==null){
+            throw new NoSuchElementException("List is empty");
+        }
+        if(last.item.equals(e)){
+            removeLast();
+            return;
+        }
+        Node current = first;
+        Node temp = first;
+        Node prev = first;
+        int flag=0;
+        while(current.next!= null){
+            if(current.item.equals(e)){
+                temp=prev;
+                size--;
+
+            }
+            current = current.next;
+            if (flag==1){
+                prev=prev.next;
+            }
+            flag=1;
+        }
+        current = temp;
+        if(current==first){
+            removeFirst();
+            return;
+        }
+        if(current.next.next != null){
+            current.next = current.next.next;
+        }
+        else {
+            current.next=null;
+        }
+
+    }
+
+    public void removeElementAllOcc(Object e) {
+        Node current = first;
+        int flag=0;
+        if(last==null){
+            throw new NoSuchElementException("List is empty");
+        }
+        while(current!=null && current.next!=null) {
+            if (current.item.equals(e) && current==first) {
+                    removeFirst();
+                    current = first;
+                    continue;
+                }
+                if(current.next.item.equals(e)) {
+                if (current.next == last) {
+                    removeLast();
+                    current = last;
+                    continue;
+                } else {
+                    current.next = current.next.next;
+                    flag=1;
+                }
+            }
+                if(flag==0) {
+                    current = current.next;
+                }
+                flag=0;
+        }
+    }
+
+    public void printListSLL(){
         Node current = first;
         while(current!= null){
             if(current.next != null){
@@ -115,11 +214,136 @@ public class SingleLinkedList {
         System.out.println();
     }
 
+    public void printListDLL(){
+        Node current = first;
+        while(current!= null){
+            if(current.next != null){
+                System.out.print(current.item + "<->");
+            }else {
+                System.out.print(current.item);
+            }
+            current = current.next;
+        }
+        System.out.println();
+    }
+
     public int size() {
         return size;
     }
 
+    public  void reverse() {
+        if(size<2){
+            return;
+        }
+        Node prev = null;
+        Node present = first;
+        Node next = present.next;
+        while (present != null){
+            present.next = prev;
+            prev = present;
+            present = next;
+            if(next != null){
+                next = next.next;
+            }
+        }
+        first=prev;
+    }
 
+    public void middleElement(){
+        if(size==0 || size==1){
+            throw new NoSuchElementException("List size should be greater than 1");
+        }
+        Node current = first;
+        int mid = size/2;
+        for(int i=0; i<mid; i++){
+            current=current.next;
+        }
+        System.out.println("Middle element: "+current.item);
+    }
+
+    public boolean cycle(){
+        Node slow = first;
+        Node fast = first;
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static SingleLinkedList mergeSort(SingleLinkedList list1, SingleLinkedList list2){
+        SingleLinkedList merged = new SingleLinkedList();
+        Node current1 = list1.first;
+        Node current2 = list2.first;
+        while(current1!=null && current2!=null){
+            if((Integer)current1.item < (Integer)current2.item){
+                merged.addLast(current1.item);
+                current1 = current1.next;
+            } else {
+                merged.addLast(current2.item);
+                current2 = current2.next;
+            }
+        }
+        while(current1!=null){
+            merged.addLast(current1.item);
+            current1 = current1.next;
+        }
+        while(current2!=null){
+            merged.addLast(current2.item);
+            current2 = current2.next;
+        }
+        return merged;
+    }
+
+    public int findFirst(Object e){
+        int index = 0;
+        Node current = first;
+        while(current!=null){
+            if(current.item.equals(e)){
+                return index;
+            }
+            current = current.next;
+            index++;
+        }
+        return -1;
+    }
+
+    public int findLast(Object e){
+        int index = 0;
+        int temp = -1;
+        Node current = first;
+        while(current!=null){
+            if(current.item.equals(e)){
+                temp = index;
+            }
+            current = current.next;
+            index++;
+        }
+        return temp;
+    }
+
+    public ArrayList<Integer> findAll(Object e){
+        ArrayList<Integer> listOfIndex = new ArrayList<>();
+        Node current = first;
+        int index = 0;
+        while(current!=null){
+            if(current.item.equals(e)){
+                listOfIndex.add(index);
+            }
+            current = current.next;
+            index++;
+        }
+        return listOfIndex;
+    }
+
+    public void clear(){
+        first = null;
+        last = null;
+        size = 0;
+    }
 
     public static class NoSuchElementException extends RuntimeException {
 
@@ -131,16 +355,15 @@ public class SingleLinkedList {
         }
     }
 
-
-    //main
     public static void main(String[] args) {
 
         SingleLinkedList list = new SingleLinkedList();
 
-        list.add("one");
-        list.add("two");
+        list.addFirst("one");
+        list.addLast("two");
         System.out.println(list.size());
-        list.printList();
+        list.printListSLL();
+
     }
 
 }
--- src/test/java/SingleLinkedListTest.java
+++ src/test/java/SingleLinkedListTest.java
@@ -2,19 +2,47 @@ import assignment_5Sept.SingleLinkedList;
 
 public class SingleLinkedListTest {
     public static void main(String[] args) {
-        SingleLinkedList list = new SingleLinkedList();
-        list.addFirst("one");
-        list.addFirst("two");
-        list.addLast("three");
-        System.out.println(list.size());
-        list.printList();
-        System.out.println(list.getFirst());
-        System.out.println(list.getLast());
-        list.removeFirst();
-        System.out.println(list.size());
-        list.printList();
-        list.removeLast();
-        System.out.println(list.size());
-        list.printList();
+//        SingleLinkedList list = new SingleLinkedList();
+//        list.addFirst("one");
+//        list.addFirst("two");
+//        list.addLast("three");
+//        list.addFirst("four");
+//        list.addLast("five");
+//        System.out.println(list.size());
+//        list.printListSLL();
+//        System.out.println(list.getFirst());
+//        System.out.println(list.getLast());
+//        list.removeFirst();
+//        System.out.println(list.size());
+//        list.printListSLL();
+//        list.removeLast();
+//        System.out.println(list.size());
+//        list.printListSLL();
+//        list.reverse();
+//        list.printListSLL();
+//        list.middleElement();
+//        System.out.println(list.cycle());
+
+        SingleLinkedList list1 = new SingleLinkedList();
+        list1.addFirst(1);
+        list1.addLast(7);
+        SingleLinkedList list2 = new SingleLinkedList();
+        list2.addFirst(4);
+        list2.addLast(5);
+        list2.addLast(5);
+//        SingleLinkedList list = SingleLinkedList.mergeSort(list1, list2);
+//        list.printListSLL();
+//        System.out.println(list.findLast(4));
+//        System.out.println(list.findAll(4));
+        list2.addLast(4);
+        list2.addLast(5);
+        list2.addLast(4);
+        list2.addLast(4);
+        list2.printListSLL();
+        list2.removeElementAllOcc(4);
+//        list2.removeFirst();
+//        list2.removeFirst();
+//        list2.removeFirst();
+        list2.printListSLL();
     }
 }
