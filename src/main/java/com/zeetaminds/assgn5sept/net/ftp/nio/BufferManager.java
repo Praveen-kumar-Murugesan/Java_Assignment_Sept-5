520985f98da0c2abba312aac4c0aa8060e034d0b
M PRAVEEN KUMAR <praveenmurugesan02@gmail.com>
2024-10-03 18:25:26 +0530

    commit56

---
+++ src/main/java/com/zeetaminds/assgn5sept/net/ftp/nio/BufferManager.java
@@ -0,0 +1,37 @@
+package com.zeetaminds.assgn5sept.net.ftp.nio;
+
+import java.nio.ByteBuffer;
+
+public class BufferManager {
+    private final ByteBuffer buffer;
+    private boolean expectingFileContent = false;
+    private String currentPutFilename;
+
+    public BufferManager(int size) {
+        this.buffer = ByteBuffer.allocate(size);
+    }
+
+    public ByteBuffer getBuffer() {
+        return buffer;
+    }
+
+    public boolean isExpectingFileContent() {
+        return expectingFileContent;
+    }
+
+    public void setExpectingFileContent(boolean expectingFileContent) {
+        this.expectingFileContent = expectingFileContent;
+    }
+
+    public String getCurrentPutFilename() {
+        return currentPutFilename;
+    }
+
+    public void setCurrentPutFilename(String currentPutFilename) {
+        this.currentPutFilename = currentPutFilename;
+    }
+
+    public void clearBuffer() {
+        buffer.clear();
+    }
+}
